8e642c8d3798c715ac1060f3cb474d35afc07a77
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2021-05-29 15:48:20 -0700

    Use Koa for the server

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const http = require('http')
+'use strict'
+
+const Koa = require('koa')
+const app = new Koa
 
 const hostname = '0.0.0.0'
 const port = 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World\n')
+app.use(ctx => {
+  ctx.body = 'Hello Koa\n'
 })
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
 })
