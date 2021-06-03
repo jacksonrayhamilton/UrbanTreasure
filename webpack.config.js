7f991888dcb2c460830d70f624a9b47473dad9d8
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2021-06-03 08:34:44 -0700

    Add CSS reset

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/
