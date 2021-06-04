566b548cefae2d20fa776a556d63daf11acaeed0
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2021-06-04 15:22:25 -0700

    Move UI src/dest files to subdirs
    This will separate UI code from API code.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: './src/ui/index.tsx',
   devtool: 'inline-source-map',
   module: {
     rules: [
@@ -21,6 +21,6 @@ module.exports = {
   },
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist/public_html')
   }
 }
