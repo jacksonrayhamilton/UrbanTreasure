ebdd498d265ae9e0d460aaa519a33397ee1f62eb
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2021-06-06 16:27:15 -0700

    Update the input value when the URL changes

--- src/ui/GameCode.tsx
+++ src/ui/GameCode.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useHistory, useParams } from 'react-router-dom'
 import styled from 'styled-components'
 
@@ -13,17 +13,27 @@ const GID_SIZE = 4
 export default function GameCode () {
   const history = useHistory()
   const { gid } = useParams<{ gid: string }>()
+  const [lastGid, setLastGid] = useState(gid)
+  const [lastInputValue, setLastInputValue] = useState(lastGid)
 
   function handleChange (e: React.ChangeEvent<HTMLInputElement>) {
     const newGid = e.target.value
+    setLastInputValue(newGid)
     if (newGid.length === GID_SIZE) history.push(`/game/${newGid}/search`)
   }
+
+  // Update the input value when the URL changes.
+  useEffect(() => {
+    if (gid === lastGid) return
+    setLastGid(gid)
+    setLastInputValue(gid)
+  })
 
   return (
     <span>
       Game Code: <Input
                    onChange={handleChange}
-                   defaultValue={gid}
+                   value={lastInputValue}
                    maxLength={4} />
     </span>
   )
