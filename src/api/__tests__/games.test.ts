9bed9274f679669fd9ccfc9651754cce73381430
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2021-06-13 14:28:12 -0700

    Add failing tests for "GET /api/games/:id"

--- src/api/__tests__/games.test.ts
+++ src/api/__tests__/games.test.ts
@@ -9,6 +9,20 @@ jest.mock('../db')
 const app = new Koa
 app.use(games.routes())
 
+describe('GET /api/games/:id', () => {
+  // TODO: Disable mocks here and use in-memory MongoDB.
+
+  it('responds with 404 when the game doesn\'t exist', async () => {
+    const response = await request(app.callback()).post('/api/games/G404')
+    expect(response.status).toBe(404)
+  })
+
+  it('responds with the game that exists', async () => {
+    const response = await request(app.callback()).post('/api/games/G200')
+    expect(response.status).toBe(200)
+  })
+})
+
 describe('POST /api/games/new', () => {
   it('creates a new game', async () => {
     const response = await request(app.callback()).post('/api/games/new')
