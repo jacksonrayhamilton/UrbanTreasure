72cd3984b1e09415db6569e5358e482d706d9668
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2021-06-16 10:07:57 -0700

    Serialize clues according to provided addresses

--- src/api/games.ts
+++ src/api/games.ts
@@ -1,4 +1,5 @@
 import Router from 'koa-router'
+import { ensureArray } from './util'
 import { Game, Clue, Address } from './types'
 import { getGamesCollection } from './db'
 import createGame from './createGame'
@@ -14,12 +15,29 @@ async function createAndSaveGame() {
   return game
 }
 
-function serializeGame(game: Game) {
+interface SerializeGameOptions {
+  clueAddresses?: string[]
+}
+
+function serializeGame(game: Game, options: SerializeGameOptions = {}) {
+  const { clueAddresses = [] } = options
   return {
     id: game.id,
-    clues: game.clues.slice(0, 1).map((clue: Clue) => clue.clue),
+    clues: serializeClues(game.clues, clueAddresses),
     addresses: game.addresses.map((address: Address) => address.address)
   }
+}
+
+function serializeClues(clues: Clue[], clueAddresses: string[]) {
+  const returnClues = clues.slice(0, 1)
+  clueAddresses.forEach((clueAddress) => {
+    for (let i = 0; i < clues.length; i++) {
+      if (clues[i].origin !== clueAddress) continue
+      returnClues[i] = clues[i]
+      break
+    }
+  })
+  return returnClues.map((clue) => clue.clue)
 }
 
 router.get('/latest', async (ctx) => {
@@ -42,9 +60,10 @@ router.get('/:id', async (ctx) => {
     ctx.response.status = 404
     return
   }
+  const clueAddresses: string[] = ensureArray(ctx.query.clue)
   ctx.body = {
     data: {
-      game: serializeGame(game)
+      game: serializeGame(game, { clueAddresses })
     }
   }
 })
--- src/api/util.ts
+++ src/api/util.ts
@@ -5,3 +5,8 @@ export function randomNumber(min: number, max: number) {
 export function randomValue(collection: any[] | string) {
   return collection[randomNumber(0, collection.length)]
 }
+
+export function ensureArray(value: any[] | any) {
+  if (Array.isArray(value)) return value
+  return [value]
+}
