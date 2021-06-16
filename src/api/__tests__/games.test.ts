384b36bbc760a0881b69b15f49949793c00672d7
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2021-06-16 10:09:19 -0700

    It omits intermediate unknown clues

--- src/api/__tests__/games.test.ts
+++ src/api/__tests__/games.test.ts
@@ -62,6 +62,19 @@ describe('GET /api/games/:id', () => {
     expect(response.body.data.game.clues[1]).toBe('Green')
     expect(response.body.data.game.clues[2]).toBe('Red')
   })
+
+  it('omits intermediate unknown clues', async () => {
+    const response =
+      await request(app.callback())
+        .get('/api/games/G200' +
+          `?clue=${encodeURIComponent('2222 Greenish Lane')}`)
+    expect(response.body).toMatchObject({ data: { game: {} } })
+    expect(Array.isArray(response.body.data.game.clues)).toBe(true)
+    expect(response.body.data.game.clues).toHaveLength(3)
+    expect(response.body.data.game.clues[0]).toBe('Blue')
+    expect(response.body.data.game.clues[1]).toBe(null)
+    expect(response.body.data.game.clues[2]).toBe('Red')
+  })
 })
 
 describe('POST /api/games/new', () => {
