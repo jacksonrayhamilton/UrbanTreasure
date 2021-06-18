a5d8353792cd355831908801e14f2ad88e5c6900
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2021-06-17 22:17:16 -0700

    Switch between word and math clues

--- src/api/createGame.ts
+++ src/api/createGame.ts
@@ -65,7 +65,72 @@ function generateClues(addresses: Address[]) {
 }
 
 function generateClue(address: Address): GameClue {
-  const associations = wordAssociations[address.streetName]
-  const clue = randomValue(associations)
+  const clue = Math.random() < .5
+    ? wordClue(address)
+    : mathClue(address)
   return { origin: null, clue }
+}
+
+function wordClue(address: Address) {
+  const associations = wordAssociations[address.streetName]
+  return randomValue(associations)
+}
+
+function mathClue(address: Address) {
+  const n = address.streetNumber
+  const roll = Math.random()
+  if (isPrime(n)) {
+    return roll < .33
+      ? additionClue(n) : roll < .66
+      ? subtractionClue(n)
+      : divisionClue(n)
+  }
+  return roll < .25
+    ? additionClue(n) : roll < .5
+    ? subtractionClue(n) : roll < .75
+    ? multiplicationClue(n)
+    : divisionClue(n)
+}
+
+function additionClue(n: number) {
+  const r = randomNumber(1, n)
+  const addend = n - r
+  return `${r}+${addend}=`
+}
+
+function subtractionClue(n: number) {
+  const r = randomNumber(1, 10000)
+  const minuend = n + r
+  return `${minuend}-${r}=`
+}
+
+function multiplicationClue(n: number) {
+  const factors = getFactors(n)
+  if (!factors.length) return 'Prime number'
+  const r = randomValue(factors)
+  const factor = Math.round(n / r)
+  return `${r}×${factor}=`
+}
+
+function divisionClue(n: number) {
+  const r = randomValue([2, 3, 4, 5, 10])
+  const dividend = n * r
+  return `${dividend}÷${r}=`
+}
+
+function isPrime(n: number) {
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) return false
+  }
+  return true
+}
+
+function getFactors(n: number) {
+  const factors = []
+  for (let i = 2; i < n; i++) {
+    if (n % i !== 0) continue
+    factors.push(i)
+    if (factors.length >= 5) break
+  }
+  return factors
 }
