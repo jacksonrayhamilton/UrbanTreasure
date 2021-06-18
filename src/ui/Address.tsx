cf9a19cabb07150af63e08e68ade682fedd5e5aa
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2021-06-17 22:48:32 -0700

    Add a back button to the address page

--- src/ui/Address.tsx
+++ src/ui/Address.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useHistory, useParams } from 'react-router-dom'
 
 import { useAppSelector, useAppDispatch } from './hooks'
 import { updateClues } from './gamesSlice'
 import { fetchAddress } from './addressesSlice'
 
 export default function Address() {
+  const history = useHistory()
   const { gid, address: addressParam } =
     useParams<{ gid: string, address: string }>()
   const clues = useAppSelector(({ games }) => games.games[gid].clues)
@@ -29,15 +30,21 @@ export default function Address() {
     dispatch(updateClues({ gid, clue, address: addressParam }))
   })
 
+  function goBack() {
+    history.goBack()
+  }
+
   if (!routedAddress) return null
   const { address, clue: clueObject, treasure } = routedAddress
   const clue = clueObject?.clue
   return (
     <>
       <h1>{address}</h1>
-      {!(clue || treasure) ? <div>Nothing to see here.</div> : null}
-      {clue ? <div><b>Clue:</b> “{clue}”</div> : null}
-      {treasure ? <div>Congrats, you found the treasure!</div> : null}
+      {!(clue || treasure) ? <p>Nothing to see here.</p> : null}
+      {clue ? <p><b>Clue:</b> “{clue}”</p> : null}
+      {treasure
+      ? <p>Congrats, you found the treasure!</p>
+      : <button onClick={goBack}>Go back</button>}
     </>
   )
 }
