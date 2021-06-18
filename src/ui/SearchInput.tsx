c5c8c6d5837ba1490c28f5acf48c7ed023b7c756
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2021-06-17 19:36:20 -0700

    Make SearchInput functional

--- src/ui/SearchInput.tsx
+++ src/ui/SearchInput.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useHistory, useParams } from 'react-router-dom'
 import styled from 'styled-components'
 
 import { Game } from './types'
@@ -37,9 +38,20 @@ interface SearchInputProps {
 }
 
 export default function SearchInput({ game }: SearchInputProps) {
+  const history = useHistory()
+  const { gid } = useParams<{ gid: string }>()
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const term = e.target.value
+    const params = new URLSearchParams
+    if (term) params.set('term', term)
+    const query = params.toString() ? `?${params}` : ''
+    history.push(`/game/${gid}/search${query}`)
+  }
+
   return (
     <InputContainer isHidden={!game}>
-      <Input type="search" />
+      <Input type="search" onChange={handleChange} />
       <IconContainer>
         <SearchIcon />
       </IconContainer>
